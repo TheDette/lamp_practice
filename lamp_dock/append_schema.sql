eaad91693a628832bd334a68cf2ac2b5fef3d5f7
Naoki Yoshihara <detteiuman@gmail.com>
2021-01-04 01:59:50 +0900

    新規追加するテーブルを生成するSQL文を追加 #10

---
+++ lamp_dock/append_schema.sql
@@ -0,0 +1,37 @@
+-- 購入履歴、明細画面で必要なテーブルの作成
+-- テーブル名 orders
+-- 1.order_id(注文番号) 2.user_id 3.created(購入日時) 4.updated
+-- テーブル名 order_items
+-- 1.order_id 2.item_id 3.amount 4.created 5.updated
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `orders`
+--
+
+CREATE TABLE `orders` (
+  `order_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  primary key('order_id')
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `order_items`
+--
+
+CREATE TABLE `order_items` (
+  `order_id` int(11) NOT NULL,
+  `item_id` int(11) NOT NULL,
+  `amount` int(11) NOT NULL,
+  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
